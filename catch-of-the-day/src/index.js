44f29c61ec5f930ae8a94f92e796a52e21452249
Carla Urrea Stabile <carla.urrea@xing.com>
2019-05-23 13:16:38 +0200

    Build basic components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom'
+// Curly braces = we only need one method, kinda cherry picking the method
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+
+import './css/style.css';
+
+render(<App/>, document.querySelector('#main'));
